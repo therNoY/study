7ee5f9c6ae9857111287aeae9513a0f298d4163a
therNoY <1905190086@qq.com>
2020-08-27 11:01:21 +0800

    新增code332 新增编辑距离的解题

--- src/main/java/leetCode/tags/dynamic_programming/Code72编辑距离.java
+++ src/main/java/leetCode/tags/dynamic_programming/Code72编辑距离.java
@@ -15,7 +15,6 @@ package leetCode.tags.dynamic_programming;
  * rorse -> rose (删除 'r')
  * rose -> ros (删除 'e')
  * 示例 2：
- *
  * 输入：word1 = "intention", word2 = "execution"
  * 输出：5
  * 解释：
@@ -28,14 +27,40 @@ package leetCode.tags.dynamic_programming;
 public class Code72编辑距离 {
 
     public static void main(String[] args) {
-        String word1 = "intention";
-        String word2 = "execution";
+        String word1 = "intention", word2 = "execution";
 //        String word1 = "horse", word2 = "ros";
         System.out.println(new Code72编辑距离().minDistance(word1, word2));
 
     }
 
     public int minDistance(String word1, String word2) {
+        /**
+         * dp[i][j] 表示 将0-i 转成0-j需要的最小转换次数
+         */
+        int[][] dp = new int[word1.length()][word2.length()];
+        for (int i = 0; i < word1.length(); i++) {
+            for (int j = 0; j < word2.length(); j++) {
+                boolean isEquals = word1.charAt(i) == word2.charAt(j);
+                if (i == 0 || j == 0) {
+                    if (isEquals) {
+                        dp[i][j] = Math.max(i, j);
+                    }else {
+                        if (i == 0 && j == 0) dp[i][j] = 1;
+                        else if (i == 0) dp[i][j] = dp[i][j - 1] + 1;
+                        else if (j == 0) dp[i][j] = dp[i - 1][j] + 1;
+                    }
+                } else {
+                    if (isEquals) {
+                        dp[i][j] = dp[i - 1][j - 1];
+                    }else {
+                        dp[i][j] = min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1]) + 1;
+                    }
+                }
+            }
+        }
+        return dp[word1.length() - 1][word2.length() - 1];
+    }
+    public int minDistance2(String word1, String word2) {
         int[][] dp = new int[word1.length() + 1][word2.length() + 1];
         for (int i = 0; i <= word1.length() ; i++) {
             for (int j = 0; j <= word2.length(); j++) {
